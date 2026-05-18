42f17fa8d666f15ced14e4d9f03c34e960ea1391
Rob Bickley <robert.john.bickley@gmail.com>
2026-05-18 13:58:15 -0400

    Add signup function

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -9,10 +9,32 @@ export function AuthProvider({ children }) {
   const [location, setLocation] = useState("GATE");
 
   // TODO: signup
+  async function signup(username, password) {
+    try {
+      const response = await fetch(API + "/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: username,
+          password: password,
+        }),
+      });
+      const result = await response.json();
+      if (result.success === true) {
+        setToken(result.token);
+        setLocation("TABLET");
+      }
+      return result;
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   // TODO: authenticate
 
-  const value = { location };
+  const value = { location, signup, token };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
