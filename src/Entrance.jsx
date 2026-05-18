7c1988e0dfb2b78a55e3df1dbc241aaae6659178
Rob Bickley <robert.john.bickley@gmail.com>
2026-05-18 14:10:02 -0400

    Add newPlayer function

--- src/Entrance.jsx
+++ src/Entrance.jsx
@@ -1,6 +1,13 @@
+import { useAuth } from "./AuthContext.jsx";
+
 /** Users can enter their name to receive a token from the API. */
 export default function Entrance() {
   // TODO: call signup when form is submitted
+  const { signup } = useAuth();
+
+  const newPlayer = (formData) => {
+    signup(formData.get("name"));
+  };
 
   return (
     <>
@@ -15,7 +22,7 @@ export default function Entrance() {
         fixed on you. The one on the left opens its mouth, and with a deep,
         rumbling voice, it asks, "Who approaches? Speak your name."
       </p>
-      <form>
+      <form action={newPlayer}>
         <label>
           Name
           <input name="name" />
